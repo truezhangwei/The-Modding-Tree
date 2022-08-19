3bec9d3ac314482493072020956d93fd805e9a7f
truezhangwei <84888878+truezhangwei@users.noreply.github.com>
2022-08-19 22:43:30 +0300

    id thingy

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
 	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	id: "zhangwei",
+	author: "zhangwei",
 	pointsName: "Star Particles",
 	modFiles: ["layers.js", "tree.js"],
 
